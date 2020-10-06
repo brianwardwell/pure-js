e6deb89ab82315e2815be882c8bca05ed0e5f30e
Brian Wardwell <bwardwell79@gmail.com>
2020-10-06 16:22:49 -0700

    finished grid and add X functionality

---
+++ index.js
@@ -0,0 +1,7 @@
+// let box1 = document.getElementsByClassName('box1')
+let bOne = document.getElementById('bizzle')
+
+bOne.addEventListener('click', () => {
+    console.log('tester')
+    bOne.innerHTML = "X";
+})
